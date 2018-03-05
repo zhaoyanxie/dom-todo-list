729b9e5335578c19c39c31fbc9b1ce93d98b0c2f
davidtan <davidtan@thoughtworks.com>
2018-03-05 18:54:38 +0800

    Add solutions

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-tasks = ["buy milk", "eat dinner", "nail javascript"]
+var tasks = ["buy milk", "eat dinner", "nail javascript"];
+var todoList = document.getElementById("todo-list");
+
+// 1. display existing todos
+for (var i = 0; i < tasks.length; i++) {
+  createAndDisplayItem(tasks[i]);
+}
+
+function createAndDisplayItem(todoContent) {
+  todo = document.createElement("li");
+  todo.textContent = todoContent;
+  todo.addEventListener("click", markAsDone);
+  todoList.appendChild(todo);
+}
+
+function markAsDone(event) {
+  event.target.classList.add("done");
+}
+
+// 2. getting user input and displaying new todo
+var form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var userInput = form.elements["new todo"].value;
+  createAndDisplayItem(userInput);
+
+  form.elements["new todo"].value = "";
+});
